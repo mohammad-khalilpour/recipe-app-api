68d746e250d377ad0c77edd04541dd2dcad5c6fb
mohammad-khalilpour <kian.khalilpour@gmail.com>
2023-07-22 17:14:06 +0330

    added base class and tests for the command to wait for database to start

---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,9 @@
+"""
+ command to wait for database start
+"""
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        pass
