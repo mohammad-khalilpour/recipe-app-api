e28ce10990e8c5d03aed770d558214f76f51ab0a
mohammad-khalilpour <kian.khalilpour@gmail.com>
2023-07-23 13:39:41 +0330

    added normalization and checking if email is empty and added their tests

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -13,3 +13,24 @@ class UserTests(TestCase):
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_email_normalization(self):
+        sample_emails = [
+            ['test1@eXample.com', 'test1@example.com'],
+            ['tEst2@Example.com', 'tEst2@example.com'],
+            ['TEST3@EXAMPLE.COM', 'TEST3@example.com'],
+            ['Test4@ExamPle.com', 'Test4@example.com'],
+        ]
+        for email, expected in sample_emails:
+            user = get_user_model().objects.create_user(
+                email=email,
+                password='example123'
+            )
+            self.assertEqual(user.email, expected)
+
+    def test_user_without_email_raises_error(self):
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(
+                email='',
+                password='example123'
+            )
